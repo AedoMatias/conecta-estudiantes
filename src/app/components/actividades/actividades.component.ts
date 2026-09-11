0c165b9ba0da2592c8fd7a11049b3b7b62fc570c
AedoMatias <matias.aedo2@virginiogomez.cl>
2026-09-11 17:19:26 -0300

    commit 3 Formulario y vista de actividades

--- src/app/components/actividades/actividades.component.ts
+++ src/app/components/actividades/actividades.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-actividades',
   standalone: true,
-  imports: [],
   templateUrl: './actividades.component.html',
   styleUrl: './actividades.component.css'
 })
 export class ActividadesComponent {
-
+  actividades = [
+    { nombre: 'Taller de Programación Web', descripcion: 'Desarrollo de aplicaciones SPA con Angular 22.', horario: 'Lunes 15:00 hrs' },
+    { nombre: 'Taller de Robótica e IoT', descripcion: 'Programación de microcontroladores y sensores.', horario: 'Martes 10:00 hrs' },
+    { nombre: 'Club de Debate y Oratoria', descripcion: 'Desarrollo de habilidades de argumentación.', horario: 'Miércoles 16:00 hrs' },
+    { nombre: 'Seminario de Inteligencia Artificial', descripcion: 'Introducción a modelos de lenguaje y automatización.', horario: 'Jueves 14:00 hrs' }
+  ];
 }
--- src/app/components/inscripcion/inscripcion.component.ts
+++ src/app/components/inscripcion/inscripcion.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inscripcion',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inscripcion.component.html',
   styleUrl: './inscripcion.component.css'
 })
 export class InscripcionComponent {
+  formInscripcion = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    apellido: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    carrera: new FormControl('', [Validators.required]),
+    actividad: new FormControl('', [Validators.required]),
+    jornada: new FormControl('', [Validators.required]),
+    comentario: new FormControl('')
+  });
 
+  enviarSolicitud() {
+    if (this.formInscripcion.valid) {
+      console.log('Solicitud enviada:', this.formInscripcion.value);
+      alert('¡Solicitud de inscripción registrada con éxito!');
+      this.formInscripcion.reset();
+    } else {
+      alert('Debe completar correctamente los campos obligatorios.');
+      this.formInscripcion.markAllAsTouched();
+    }
+  }
 }
