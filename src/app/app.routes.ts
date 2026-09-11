0cfae61e4bebf5f0d620d86539215b32b725da1f
AedoMatias <matias.aedo2@virginiogomez.cl>
2026-09-11 17:05:01 -0300

    Etapa 2 Router y routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ActividadesComponent } from './components/actividades/actividades.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { InscripcionComponent } from './components/inscripcion/inscripcion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'actividades', component: ActividadesComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'inscripcion', component: InscripcionComponent },
+  { path: '**', redirectTo: 'inicio' }
+];
